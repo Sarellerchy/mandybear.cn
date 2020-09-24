bc021f5f6d82a88120636c3d6ff16077e5e33914
mandybear <mandybear319@163.com>
2020-09-25 01:52:17 +0800

    mandybear

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask, request, redirect, url_for, render_template,flash
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from login_models import User, query_user
 
 app = Flask(__name__)
+app.secret_key = '1234567'
 
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+login_manager.login_message = '请登录'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    if query_user(user_id) is not None:
+        curr_user = User()
+        curr_user.id = user_id
+
+        return curr_user
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+@login_required
+def index():
+    return render_template('tool_ciyuntu.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_id = request.form.get('username')
+        user = query_user(user_id)
+        if user is not None and request.form['password'] == user['password']:
+
+            curr_user = User()
+            curr_user.id = user_id
+
+            # 通过Flask-Login的login_user方法登录用户
+            login_user(curr_user)
+
+            return redirect(url_for('index'))
+
+        flash('用户名或密码错误!')
+
+    # GET 请求
+    return render_template('index.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'Logged out successfully!'
 
 
 if __name__ == '__main__':
