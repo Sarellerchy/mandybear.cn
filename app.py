917dcc57dbfe09457498f3f537c5e84ea6daa4d3
mandybear <mandybear319@163.com>
2020-09-27 00:28:08 +0800

    mandybear

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, url_for, render_template,flash
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from login_models import User, query_user
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = '1234567'
@@ -16,13 +17,15 @@ def load_user(user_id):
     if query_user(user_id) is not None:
         curr_user = User()
         curr_user.id = user_id
-
         return curr_user
 
 @app.route('/')
 @login_required
 def index():
-    return render_template('tool_ciyuntu.html')
+    dataciyuntudefault = pd.read_excel("./source/datadefault/词云图数据.xlsx")
+    dataciyuntudefault.columns=['label','value']
+    dataciyuntudefault_json = dataciyuntudefault.to_dict(orient='records')
+    return render_template('tool_ciyuntu.html',wordclouddata = dataciyuntudefault_json)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -48,8 +51,18 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return 'Logged out successfully!'
+    return render_template('index.html')
 
+@app.route('/data_get', methods=['POST'])
+def data_get():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        filename = file.filename
+        file.save("./source/datasave/ciyuntudata."+filename.split('.')[1])
+    data_ciyuntu = pd.read_excel("./source/datasave/ciyuntudata."+filename.split('.')[1])
+    data_ciyuntu.columns = ['label', 'value']
+    data_ciyuntu_json = data_ciyuntu.to_dict(orient='records')
+    return render_template('tool_ciyuntu.html', wordclouddata=data_ciyuntu_json)
 
 if __name__ == '__main__':
     app.run()
